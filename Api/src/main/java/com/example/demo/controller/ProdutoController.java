0a7083f7fce516c9fef61c711ce7d75250edb357
Lucas Borges <lucas-borges2000@hotmail.com>
2024-11-24 17:01:21 -0300

    produto funcionando

--- Api/src/main/java/com/example/demo/controller/ProdutoController.java
+++ Api/src/main/java/com/example/demo/controller/ProdutoController.java
@@ -13,87 +13,77 @@ import jakarta.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins="http://127.0.0.1:5500")
 @RestController
-@RequestMapping("/produtos") 
+@RequestMapping("/produtos")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class ProdutoController {
 
     @Autowired
     private ProdutoRepository produtoRepository;
-
 
     @GetMapping
     public List<Produto> listarTodos() {
         return produtoRepository.findAll();
     }
 
-  
-	@GetMapping("/produto/{id}")
-	public ResponseEntity<Produto>GetBydId(@PathVariable long id){
-		
-		Optional<Produto> produto = produtoRepository.findById(id);
-		
-		if(produto.isPresent())
-			return new ResponseEntity<Produto>(produto.get(), HttpStatus.OK);
-		else
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		
-	}
-	
-	
-	@PostMapping("/produto")
-	public ResponseEntity<Response<Produto>> Post(@Valid @RequestBody Produto produto, BindingResult result){
-		
-		Response<Produto>response = new Response<Produto>();
-		
-		if(result.hasErrors()) {
-			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
-			return ResponseEntity.badRequest().body(response);
-		}
-		
-		produtoRepository.save(produto);
-		response.setData(produto);
-		return ResponseEntity.ok(response);
-	}
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> buscarPorId(@PathVariable long id) {
+        Optional<Produto> produto = produtoRepository.findById(id);
 
-   
-	@PutMapping("/pessoa/{id}")
-	public ResponseEntity<Response<Produto>>Put(@PathVariable long id,@Valid @RequestBody Produto newProduto, BindingResult result){
-		
-		Optional<Produto> oldProduto = produtoRepository.findById(id);
-		Response<Produto> response = new Response<Produto>();
-		
-		if(result.hasErrors()) {
-			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
-			return ResponseEntity.badRequest().body(response);
-		}
-		
-		if(oldProduto.isPresent()) {
-			Produto produto = oldProduto.get();
-			produto.setNome(newProduto.getNome());			
-			produto.setPreco(newProduto.getPreco());
-			produto.setQuantidade(newProduto.getQuantidade());
-			produtoRepository.save(produto);
-			response.setData(produto);
-			
-			return ResponseEntity.ok(response);			
-		}
-		else 
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	}
-  
+        if (produto.isPresent()) {
+            return new ResponseEntity<>(produto.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
-	@DeleteMapping("/produto/{id}")
-	public ResponseEntity<Object>Delete(@PathVariable long id){
-		
-		Optional<Produto>produto = produtoRepository.findById(id);
-		
-		if(produto.isPresent()){
-			produtoRepository.delete(produto.get());
-			
-			return new ResponseEntity<>(HttpStatus.OK);
-		}
-		else
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	}
+    @PostMapping
+    public ResponseEntity<Response<Produto>> criar(@Valid @RequestBody Produto produto, BindingResult result) {
+        Response<Produto> response = new Response<>();
+
+        if (result.hasErrors()) {
+            result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        produtoRepository.save(produto);
+        response.setData(produto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Response<Produto>> atualizar(@PathVariable long id, @Valid @RequestBody Produto novoProduto, BindingResult result) {
+        Response<Produto> response = new Response<>();
+
+        if (result.hasErrors()) {
+            result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        Optional<Produto> produtoExistente = produtoRepository.findById(id);
+
+        if (produtoExistente.isPresent()) {
+            Produto produto = produtoExistente.get();
+            produto.setNome(novoProduto.getNome());
+            produto.setPreco(novoProduto.getPreco());
+            produto.setQuantidade(novoProduto.getQuantidade());
+            produtoRepository.save(produto);
+            response.setData(produto);
+            return ResponseEntity.ok(response);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable long id) {
+        Optional<Produto> produto = produtoRepository.findById(id);
+
+        if (produto.isPresent()) {
+            produtoRepository.delete(produto.get());
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
 }
