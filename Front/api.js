98f2d76b84dafe1f79c254f43933f53daad094fe
Lucas Borges <lucas-borges2000@hotmail.com>
2024-11-25 22:34:17 -0300

    Versao Final Front end

--- Front/api.js
+++ Front/api.js
@@ -1,74 +1,83 @@
-const URL_BASE = "http://localhost:8080"; 
-
+const URL_BASE = "http://localhost:8080";
 
 async function listarTodos(path) {
-    const resposta = await fetch(`${URL_BASE}/${path}`, {
-        method: "GET",
-    });
+  const resposta = await fetch(`${URL_BASE}/${path}`, {
+    method: "GET",
+  });
 
-    if (!resposta.ok) {
-        throw new Error(`Erro ao listar todos: ${resposta.status}`);
-    }
+  if (!resposta.ok) {
+    const erroApi = await extrairErro(resposta);
+    throw new Error(erroApi);
+  }
 
-    return resposta.json();
+  return resposta.json();
 }
-
 
 async function buscarPorId(path, id) {
-    const resposta = await fetch(`${URL_BASE}/${path}/${id}`, {
-        method: "GET",
-    });
+  const resposta = await fetch(`${URL_BASE}/${path}/${id}`, {
+    method: "GET",
+  });
 
-    if (!resposta.ok) {
-        throw new Error(`Erro ao buscar por id: ${resposta.status}`);
-    }
+  if (!resposta.ok) {
+    const erroApi = await extrairErro(resposta);
+    throw new Error(erroApi);
+  }
 
-    return resposta.json();
+  return resposta.json();
 }
-
 
 async function enviar(path, dados) {
-    const resposta = await fetch(`${URL_BASE}/${path}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(dados),
-    });
+  const resposta = await fetch(`${URL_BASE}/${path}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dados),
+  });
 
-    if (!resposta.ok) {
-        throw new Error(`Erro ao enviar: ${resposta.status}`);
-    }
+  if (!resposta.ok) {
+    const erroApi = await extrairErro(resposta);
+    throw new Error(erroApi);
+  }
 
-    return resposta.json();
+  return resposta.json();
 }
-
 
 async function atualizar(path, id, dados) {
-    const resposta = await fetch(`${URL_BASE}/${path}/${id}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(dados),
-    });
+  const resposta = await fetch(`${URL_BASE}/${path}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dados),
+  });
 
-    if (!resposta.ok) {
-        throw new Error(`Erro ao atualizar: ${resposta.status}`);
-    }
+  if (!resposta.ok) {
+    const erroApi = await extrairErro(resposta);
+    throw new Error(erroApi);
+  }
 
-    return resposta.json();
+  return resposta.json();
 }
 
-
 async function remover(path, id) {
-    const resposta = await fetch(`${URL_BASE}/${path}/${id}`, {
-        method: "DELETE",
-    });
+  const resposta = await fetch(`${URL_BASE}/${path}/${id}`, {
+    method: "DELETE",
+  });
 
-    if (!resposta.ok) {
-        throw new Error(`Erro ao deletar: ${resposta.status}`);
-    }
+  if (!resposta.ok) {
+    const erroApi = await extrairErro(resposta);
+    throw new Error(erroApi);
+  }
+}
+
+async function extrairErro(resposta) {
+  try {
+    const erroJson = await resposta.json();
+    return erroJson.errors ? erroJson.errors : JSON.stringify(erroJson);
+  } catch {
+    return "Erro desconhecido.";
+  }
 }
 
 export { listarTodos, buscarPorId, enviar, atualizar, remover };
